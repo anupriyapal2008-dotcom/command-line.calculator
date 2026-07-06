9f2c68d8a3d6c9dd6bacd60d89d3cb460ef64e18
Anupriya Pal <anupriyapal2008@gmail.com>
2026-07-06 19:32:49 +0530

    Staged, added calculation function and repeat loop

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def calculator():
+    first_number = input("Enter the first number: ")
+    second_number = input("Enter the second number: ")
+    selection = input("Enter the operation you want to perform (+, -, *, /): ")
+    First_number = float(first_number)
+    Second_number = float(second_number)
+    if selection == "+":
+        result= First_number + Second_number
+        print("The result is: ", result)
+    elif selection == "-":
+        result= First_number - Second_number
+        print("The result is: ", result)
+    elif selection == "*":
+        result= First_number * Second_number
+        print("The result is: ", result)
+    elif selection == "/":
+        if Second_number == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            result= First_number / Second_number
+            print("The result is: ", result)
+    else:
+        print("Invalid operation selected.")
+        
+while True:
+    calculator()
+    continue_calculation = input("Do you want to perform another calculation? (yes/no): ")
+    if continue_calculation.lower() != "yes":
+        print("Thank you for using the calculator. Goodbye!")
+        break
